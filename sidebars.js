5a9e187eace49d1c2c3e7447ad2d99a2711c4157
seby <90622816+sebyaka864@users.noreply.github.com>
2026-08-28 15:19:03 +0300

    move guide to root domain routes

--- sidebars.js
+++ sidebars.js
@@ -1,34 +1,12 @@
 module.exports = {
   guideSidebar: [
     { type: 'link', label: 'home', href: '/' },
-    {
-      type: 'category',
-      label: 'get started',
-      collapsible: false,
-      collapsed: false,
-      items: [
-        { type: 'doc', id: 'intro', label: 'introduction' },
-        { type: 'doc', id: 'guide/before-you-begin', label: 'before you begin' },
-        { type: 'doc', id: 'guide/clean-install', label: 'setup' },
-      ],
-    },
-    {
-      type: 'category',
-      label: 'build a solid setup',
-      collapsible: false,
-      collapsed: false,
-      items: [
-        'guide/drivers-and-updates',
-        'guide/runtime-components',
-        'guide/optimization',
-      ],
-    },
-    {
-      type: 'category',
-      label: 'more resources',
-      collapsible: false,
-      collapsed: false,
-      items: ['guide/other-guides'],
-    },
+    { type: 'doc', id: 'intro', label: 'introduction' },
+    { type: 'doc', id: 'guide/before-you-begin', label: 'before you begin' },
+    { type: 'doc', id: 'guide/clean-install', label: 'setup' },
+    { type: 'doc', id: 'guide/drivers-and-updates', label: 'drivers and updates' },
+    { type: 'doc', id: 'guide/runtime-components', label: 'runtime components' },
+    { type: 'doc', id: 'guide/optimization', label: 'optimization' },
+    { type: 'doc', id: 'guide/other-guides', label: 'other guides' },
   ],
 };
